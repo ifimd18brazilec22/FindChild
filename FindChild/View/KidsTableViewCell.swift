67c71e042c0ad9b1f61ca30eccb1706c7265c77e
Web ᴀʀᴃɪᴏs <arbi.personal@gmail.com>
2019-10-27 07:41:57 +0300

    Added two screens

---
+++ FindChild/View/KidsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  KidsTableViewCell.swift
+//  FindChild
+//
+//  Created by Malik on 27.10.2019.
+//  Copyright © 2019 Артем Стратиенко. All rights reserved.
+//
+
+import UIKit
+
+class KidsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FindChild/View/SetupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SetupViewController.swift
+//  FindChild
+//
+//  Created by Malik on 27.10.2019.
+//  Copyright © 2019 Артем Стратиенко. All rights reserved.
+//
+
+import UIKit
+
+class SetupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
